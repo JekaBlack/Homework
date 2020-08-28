7241210de381b897280ddc1d74238262e07a564a
JekaBlack <evgenchern03@gmail.com>
2020-08-28 13:53:13 +0300

    delete 21_08_2020

--- 21_08_2020/script1_11/script1_11.js
+++
@@ -1,5 +0,0 @@
-var x = Math.round(Math.random() * 100);
-alert("Значение X: " + x);
-var y = prompt("Введите y");
-var a = +x < y;
-alert("x < y = " + a + ", " + "x = " + x + ", " + "y = " + y);
--- 21_08_2020/script1_3/script1_3.js
+++
@@ -1,3 +0,0 @@
-var firstName = prompt("Your First name", "");
-var lastName = prompt("Your Last name", "");
-alert("What's up, " + firstName.toUpperCase() + " " + lastName + "?");
